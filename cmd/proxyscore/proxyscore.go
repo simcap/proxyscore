4b643a04cc9f5b746cf21dde72a66a978f35eb6d
simcap <simcap@fastmail.com>
2015-03-26 17:16:27 +0100

    cleanup

--- cmd/proxyscore/proxyscore.go
+++ cmd/proxyscore/proxyscore.go
@@ -10,9 +10,8 @@ import (
 	"net/url"
 )
 
-const servicePort = 4673
-
-var serviceIp = []byte{0x34, 0x35, 0x2e, 0x35, 0x35, 0x2e, 0x31, 0x34, 0x34, 0x2e, 0x31, 0x36, 0x31}
+var serviceURL = "\x68\x74\x74\x70\x3a\x2f\x2f\x34\x35\x2e\x35\x35\x2e\x31\x34\x34\x2e\x31\x36\x31\x3a\x34\x36\x37\x33"
+var myIpURL = "http://api.ipify.org/?format=json"
 
 type serviceResponse struct {
 	Header     map[string][]string
@@ -25,22 +24,9 @@ func (s serviceResponse) String() string {
 
 func main() {
 	proxy := flag.String("p", "", "valid proxy ip address with port (ex: 1.2.3.4:6789)")
-	verbose := flag.Bool("v", false, "verbose in case of errors")
+	verbose := flag.Bool("v", false, "verbose mode")
 
 	flag.Parse()
-
-	resp, err := http.Get("http://api.ipify.org/?format=json")
-	if err != nil {
-		log.Fatalf("Error in service to get your IP: %s", err)
-	}
-	defer resp.Body.Close()
-
-	d := struct{ IP string }{}
-	json.NewDecoder(resp.Body).Decode(&d)
-	myip := net.ParseIP(d.IP)
-	if myip == nil {
-		log.Fatalf("Error parsing your ip %s", d.IP)
-	}
 
 	ip, proxyport, errsplit := net.SplitHostPort(*proxy)
 	if errsplit != nil {
@@ -48,7 +34,6 @@ func main() {
 	}
 
 	proxyip := net.ParseIP(ip)
-
 	if proxyip == nil {
 		log.Fatalf("invalid ip address provided")
 	}
@@ -63,7 +48,7 @@ func main() {
 	}
 	client := &http.Client{Transport: transport}
 
-	resp, errget := client.Get(fmt.Sprintf("http://%s:%d", serviceIp, servicePort))
+	resp, errget := client.Get(serviceURL)
 	if errget != nil {
 		log.Fatalf("Error contacting service: %s", errget)
 	}
@@ -82,6 +67,7 @@ func main() {
 		log.Println(result)
 	}
 
+	myip := getMyIp()
 	guilty := containsMyIP(result, myip)
 
 	if len(guilty) == 0 {
@@ -108,4 +94,20 @@ func containsMyIP(result serviceResponse, myip net.IP) []string {
 	}
 
 	return guilty
+}
+
+func getMyIp() net.IP {
+	resp, err := http.Get(myIpURL)
+	if err != nil {
+		log.Fatalf("Error '%s' service to get your IP: %s", myIpURL, err)
+	}
+	defer resp.Body.Close()
+
+	d := struct{ IP string }{}
+	json.NewDecoder(resp.Body).Decode(&d)
+	myip := net.ParseIP(d.IP)
+	if myip == nil {
+		log.Fatalf("Error parsing your ip %s from service %s", d.IP, myIpURL)
+	}
+	return myip
 }
